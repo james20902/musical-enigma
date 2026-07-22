ed5e2250650a7112c2736dd35430707358255cc1
james20902 <james20902@gmail.com>
2026-07-21 20:34:18 -0400

    updated code to feature generic subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,26 +7,11 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import frc.robot.generated.TunerConstants;
-import frc.robot.subsystems.intake.Intake;
-import frc.robot.subsystems.intake.IntakeIO;
-import frc.robot.subsystems.intake.IntakeIOSim;
-import frc.robot.subsystems.turret.QuadTurret;
-import frc.robot.subsystems.turret.Turret;
-import frc.robot.subsystems.turret.TurretIOSim;
-
-import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -35,55 +20,12 @@ import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // Subsystems
-  private final Intake intake;
-  private final QuadTurret turret;
 
   // Controller
   private final CommandXboxController controller = new CommandXboxController(0);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    turret = new QuadTurret(new TurretIOSim(), new TurretIOSim(), new TurretIOSim(), new TurretIOSim());
-    switch (Constants.currentMode) {
-      case REAL:
-        intake = new Intake(new IntakeIOSim());
-        break;
-
-      case SIM:
-        
-
-        intake = new Intake(new IntakeIOSim());
-        break;
-
-      default:
-        // Replayed robot, disable IO implementations
-        intake = new Intake(new IntakeIO() {
-            @Override
-            public void updateInputs(IntakeIOInputs inputs) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'updateInputs'");
-            }
-
-            @Override
-            public void setIntakeSpeed(double volts) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'setIntakeSpeed'");
-            }
-
-            @Override
-            public double getIntakePosRad() {
-              // TODO Auto-generated method stub
-              throw new UnsupportedOperationException("Unimplemented method 'getIntakePosRad'");
-            }
-
-            @Override
-            public void simUpdate() {
-              // TODO Auto-generated method stub
-              throw new UnsupportedOperationException("Unimplemented method 'simUpdate'");
-            }});
-        break;
-    }
 
     // Configure the button bindings
     configureButtonBindings();
@@ -98,14 +40,6 @@ public class RobotContainer {
   private void configureButtonBindings() {
     // Default command, normal field-relative drive
     
-    turret.setDefaultCommand(
-      new SequentialCommandGroup(
-        turret.setTurretSetpointRad(() ->
-          -Math.atan2(MathUtil.applyDeadband(controller.getLeftY(), 0.2),
-           MathUtil.applyDeadband(controller.getLeftX(), 0.2))
-        )
-      )
-    );
     
   }
 
---
+++ src/main/java/frc/robot/subsystems/generic/GenericIOImplementation2.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems.generic;
+
+
+// this is where implementation code goes
+// in other words, for every set of hardware/simulation, make the code
+// that actually does what you want the entire subsystem to do
+public class GenericIOImplementation2 implements GenericIO {
+
+    @Override
+    public void updateInputs(GenericIOInputs inputs) {
+
+    }
+
+    public void genericFunction() {
+        System.err.println("I behave slightly differently!");
+    }
+    
+}
